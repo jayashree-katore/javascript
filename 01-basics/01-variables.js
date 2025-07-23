d88d5cee44f015ee487f241c5843019a24ce44ed
jayashree-katore <jayashreekatore09@gmail.com>
2025-07-23 12:50:12 +0000

    datatypes

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,7 @@
+const accountId = 123456
+var accountEmail = "j@google.com"
+let acountPass = "123"
+accountCity = "Ahilyanagar"
+
+console.log(accountId);
+console.table([accountEmail,accountCity,acountPass])
