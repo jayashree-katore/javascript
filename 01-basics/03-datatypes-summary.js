2ba179ab8c539e7e5ecdd19a93468b6dedade2b8
jayashree-katore <jayashreekatore09@gmail.com>
2025-07-31 11:32:22 +0000

    add datatypes summary

---
+++ 01-basics/03-datatypes-summary.js
@@ -0,0 +1,14 @@
+const id = 123;
+const id2 = 58957766544n;
+
+console.log(typeof(id));
+console.log(typeof(id2));
+
+const fname = "Jayashree";
+const check = true;
+
+console.log(typeof(fname));
+console.log(typeof(check));
+
+
+
